0120922d30e20348cbe80ce0585a9fda0e89defa
Döring Christian <christian.doering@um.de>
2022-11-11 22:53:26 +0100

    pcg from mitsuba

--- util.py
+++ util.py
@@ -2,10 +2,13 @@ import mitsuba as mi
 import drjit as dr
 
 
-def pcg(v: mi.UInt32) -> mi.UInt32:
-    state = (v * 747796405) + 2891336453
-    word = ((state >> ((state >> 28) + 4)) ^ state) * 277803737
-    return (word >> 22) ^ word
+def pcg(state: mi.UInt32) -> mi.UInt32:
+    oldstate = state
+    state = dr.fma(oldstate, mi.UInt64(0x5851F42D4C957F2D), 0)
+    xorshift = mi.UInt32((oldstate ^ (oldstate >> 18)) >> 27)
+    rot = mi.UInt32(oldstate >> 59)
+
+    return (xorshift >> rot) | (xorshift << ((-mi.Int32(rot)) & 31))
 
 
 def uint32_to_uniform_float(v: mi.UInt32) -> mi.Float32:
